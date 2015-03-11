bdd7c580dec905d652fcc914cabfaeec24787a8d
Nico Hoffmann <github@ohnehin.com>
2015-03-11 16:27:05 +0100

    Added support for Romanian
    Thanks to https://github.com/structor fro providing the language file

---
+++ lang/ro.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'sec'   => array('1 secundă',  '|:count| secunde'),
+    'min'   => array('1 minut',    '|:count| minute'),
+    'h'     => array('1 oră',      '|:count| ore'),
+    'd'     => array('1 zi',       '|:count| zile'),
+    'w'     => array('1 săptămână','|:count| săptămâni'),
+    'm'     => array('1 lună',     '|:count| luni'),
+    'y'     => array('1 an',       '|:count| ani'),
+    'meta'  => array(
+                'after_now'     => '|:phrase| în viitor',
+                'before_now'    => '|:phrase| în urmă'
+                ),
+
+    'supports' => 0.9
+    );
